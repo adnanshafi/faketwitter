d2839b8000df32c737db0ed645420b4947d32d9e
adnanshafi <adnanshalla@gmail.com>
2022-07-13 12:12:30 +0530

    added model for User

---
+++ packages/auth/lib/src/user.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+/// [User] : Model For User
+class User extends Equatable {
+  const User({required this.uid});
+
+  /// [uid] : Stores the Firebase UID of the user
+  final String uid;
+
+  /// An Empty User
+  static const User empty = User(uid: '-1');
+
+  User copyWith({String? uid}) => User(uid: uid ?? this.uid);
+
+  @override
+  List<Object?> get props => [uid];
+}
